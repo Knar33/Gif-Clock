14142caf2b812585125834951036a8d26c7d7274
Knar33 <adbyerly@comcast.net>
2019-02-01 19:08:52 -0600

    Very basic byte streaming capability

---
+++ Gif-Clock/Models/GifStream.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Threading;
+
+namespace GifClock
+{
+    public class GifStream
+    {
+        public async void WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
+        {
+            var buffer = new byte[10];
+
+            while (true)
+            {
+                buffer[0] = 0;
+                buffer[1] = 1;
+                buffer[2] = 2;
+                await outputStream.WriteAsync(buffer, 0, 10);
+                Thread.Sleep(1000);
+            } 
+        }
+    }
+}
