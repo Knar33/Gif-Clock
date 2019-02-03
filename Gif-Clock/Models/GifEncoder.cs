492be09ba0a2af7b43f60bf185963a8cf7808cce
Knar <knarxr@protonmail.com>
2019-02-03 17:33:57 -0600

    Write the Logican Screen Descriptor

--- Gif-Clock/Models/GifEncoder.cs
+++ Gif-Clock/Models/GifEncoder.cs
@@ -16,14 +16,46 @@ namespace GifClock
         public GifEncoder(Stream inputStream, int width, int height, List<Color> globalColorTable)
         {
             gifStream = inputStream;
-            //Encode header
+            //Header
             Task.Run(() => WriteString("GIF89a")).Wait();
+
             //Logican Screen Descriptor
+            WriteShort(width); //Canvas Width
+            WriteShort(height); //Canvas Height
+            GeneratePackedField(globalColorTable.Count); //Packed Field
+            WriteByte(0); //Background Color Index
+            WriteByte(0); //Pixel Aspect Ratio
+
             //Global Color Table
         }
 
         public async Task AddFrame(Image frame)
         {
+        }
+
+        private void GeneratePackedField(int colorCount)
+        {
+            //The first bit is the Global Color Table Flag
+            //The next 3 bits are the Color Resolution, which I am setting to 1
+            //The next is the sort flag. It will always be zero for this
+            //The last 3 bits are the Global Color Table Size. The real size of the table is 2^(n+1) where n is the Global Color Table Size.
+            int packedFieldValue = 16;
+            if (colorCount > 0)
+            {
+                packedFieldValue += 128;
+            }
+
+            int globalColorTableSize = 0;
+            for (int p = 1; p < 8; p++)
+            {
+                if (colorCount > Math.Pow(2, p))
+                {
+                    globalColorTableSize++;
+                }
+            }
+            packedFieldValue += globalColorTableSize;
+
+            WriteByte(packedFieldValue);
         }
 
         private void WriteByte(int value)
