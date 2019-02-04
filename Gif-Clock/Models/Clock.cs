0c2ca4da3bc8ae65dabcbba11c5f9dbd17208749
Knar <knarxr@protonmail.com>
2019-02-03 20:48:40 -0600

    Added the Clock class, which generates the clock frames

---
+++ Gif-Clock/Models/Clock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace GifClock
+{
+    public class Clock
+    {
+        public static Image GenerateTime()
+        {
+            Image image = new Bitmap(100, 100);
+            Graphics drawing = Graphics.FromImage(image);
+            Font font = new Font("Times New Roman", 12.0f);
+            Color textColor = Color.FromArgb(255, 255, 255);
+            Color backgroundColor = Color.FromArgb(0, 0, 0);
+
+            drawing = Graphics.FromImage(image);
+            drawing.Clear(backgroundColor);
+            Brush textBrush = new SolidBrush(textColor);
+
+            drawing.DrawString(DateTime.Now.ToString("HH:mm:ss"), font, textBrush, 0, 0);
+
+            drawing.Save();
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            return image;
+        }
+    }
+}
