7b3cb92c862637e018ee43a036ece859651208cf
Knar33 <adbyerly@comcast.net>
2019-02-03 20:26:54 -0600

    Removed the dynamic Global Color Table, because I am reading from

--- Gif-Clock/Models/GifEncoder.cs
+++ Gif-Clock/Models/GifEncoder.cs
@@ -12,21 +12,14 @@ namespace GifClock
     public class GifEncoder
     {
         public Stream GifStream { get; set; }
-        private List<Color> GlobalColorTable { get; set; }
         private int Width { get; set; }
         private int Height { get; set; }
 
-        public GifEncoder(Stream inputStream, int width, int height, List<Color> globalColorTable)
+        public GifEncoder(Stream inputStream, int width, int height)
         {
-            //TODO: allow no globalColorTable to be passed in?
             Width = width;
             Height = height;
             GifStream = inputStream;
-            if (globalColorTable.Count > 256)
-            {
-                globalColorTable = globalColorTable.Take(256).ToList();
-            }
-            GlobalColorTable = globalColorTable;
 
             GenerateHeader();
         }
@@ -39,48 +32,14 @@ namespace GifClock
             //Logical Screen Descriptor
             WriteShort(Width); //Canvas Width
             WriteShort(Height); //Canvas Height
-            //Packed Field
-            int packedFieldValue = 16;
-            bool globalColorTableIndicator = false;
-            if (GlobalColorTable.Count > 0)
-            {
-                globalColorTableIndicator = true;
-                packedFieldValue += 128;
-            }
-            int globalColorTableSize = 0;
-            for (int p = 1; p < 8; p++)
-            {
-                if (GlobalColorTable.Count > Math.Pow(2, p))
-                {
-                    globalColorTableSize++;
-                }
-            }
-            packedFieldValue += globalColorTableSize;
-            WriteByte(packedFieldValue);
+            WriteByte(16); //Packed Field
             WriteByte(0); //Background Color Index
             WriteByte(0); //Pixel Aspect Ratio
-
-            //Global Color Table
-            if (globalColorTableIndicator)
-            {
-                for (int i = 0; i < GlobalColorTable.Count; i++)
-                {
-                    WriteByte(GlobalColorTable[i].R);
-                    WriteByte(GlobalColorTable[i].G);
-                    WriteByte(GlobalColorTable[i].B);
-                }
-                int colorsRequired = (int)Math.Pow(2, (globalColorTableSize + 1));
-                for (int i = GlobalColorTable.Count; i < colorsRequired; i++)
-                {
-                    WriteByte(0);
-                    WriteByte(0);
-                    WriteByte(0);
-                }
-            }
         }
 
         public async Task AddFrame(Image frame, int x, int y)
         {
+            var b = frame.PixelFormat;
             using (var sourceGif = new MemoryStream())
             {
                 frame.Save(sourceGif, ImageFormat.Gif);
