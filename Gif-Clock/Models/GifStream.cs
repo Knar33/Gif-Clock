a260e19cea79b3c4e203663b8c393e4eb1902dc5
Knar33 <adbyerly@comcast.net>
2019-02-03 20:48:40 -0600

    Added the Clock class, which generates the clock frames

--- Gif-Clock/Models/GifStream.cs
+++ Gif-Clock/Models/GifStream.cs
@@ -21,11 +21,7 @@ namespace GifClock
                 while (true)
                 {
                     //Generate clock Image frame
-                    var white = Image.FromFile(@"C:\Users\Andrew\Documents\GitHub\Gif-Clock\white.png");
-                    await encoder.AddFrame(white, 0, 0);
-                    Thread.Sleep(1000);
-                    var black = Image.FromFile(@"C:\Users\Andrew\Documents\GitHub\Gif-Clock\black.png");
-                    await encoder.AddFrame(black, 0, 0);
+                    await encoder.AddFrame(Clock.GenerateTime(), 0, 0);
                     Thread.Sleep(1000);
                 }
             }
