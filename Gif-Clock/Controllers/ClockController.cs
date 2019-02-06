024638d8554b35a7d0e555553757cff6ce36a5ce
Knar33 <adbyerly@comcast.net>
2019-02-06 01:09:37 -0600

    Created the IGenerator class, which allows injecting an ImageGenerator into the GifStream class. This will enable future extensibility

--- Gif-Clock/Controllers/ClockController.cs
+++ Gif-Clock/Controllers/ClockController.cs
@@ -14,7 +14,20 @@ namespace GifClock.Controllers
         public HttpResponseMessage Get()
         {
             var response = Request.CreateResponse();
-            Action<Stream, HttpContent, TransportContext> writeToStream = GifStream.WriteToStream;
+            Clock clock = new Clock();
+            GifStream gifStream = new GifStream(clock);
+            Action<Stream, HttpContent, TransportContext> writeToStream = gifStream.WriteToStream;
+            response.Content = new PushStreamContent(writeToStream, new MediaTypeHeaderValue("image/gif"));
+
+            return response;
+        }
+
+        public HttpResponseMessage Get(string timeZone)
+        {
+            var response = Request.CreateResponse();
+            Clock clock = new Clock(timeZone);
+            GifStream gifStream = new GifStream(clock);
+            Action<Stream, HttpContent, TransportContext> writeToStream = gifStream.WriteToStream;
             response.Content = new PushStreamContent(writeToStream, new MediaTypeHeaderValue("image/gif"));
 
             return response;
