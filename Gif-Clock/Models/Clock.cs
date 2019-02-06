96db022fd553d5c9865b6d73ad9a4278de5e04c6
Knar33 <adbyerly@comcast.net>
2019-02-06 02:39:12 -0600

    Swapped colors and added a border

--- Gif-Clock/Models/Clock.cs
+++ Gif-Clock/Models/Clock.cs
@@ -26,14 +26,16 @@ namespace GifClock
         public Image GenerateImage()
         {
             Image image = new Bitmap(62, 18);
-            Font font = new Font("Times New Roman", 12.0f);
-            Color backgroundColor = Color.FromArgb(255, 255, 255);
-            Color textColor = Color.FromArgb(0, 0, 0);
+            Font font = new Font("Calibri", 12.0f);
+            Color textColor = Color.FromArgb(255, 255, 255);
+            Color backgroundColor = Color.FromArgb(0, 0, 0);
 
             Graphics drawing = Graphics.FromImage(image);
             drawing.Clear(backgroundColor);
             Brush textBrush = new SolidBrush(textColor);
-            
+            Pen rectPen = new Pen(textBrush);
+            drawing.DrawRectangle(rectPen, new Rectangle(0, 0, 61, 17));
+
             drawing.DrawString(TimeZoneInfo.ConvertTime(DateTime.Now, timeZoneInfo).ToString("HH:mm:ss"), font, textBrush, 0, 0);
 
             drawing.Save();
